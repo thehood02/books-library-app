d3d89a3c9d2643cc0d46af93425b9a8689678c4e
thehood02 <yash28847@gmail.com>
2023-02-06 22:28:14 +0530

    add api call & table

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,64 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Books() {
   const { subject } = useParams();
+  const [counter, setCounter] = useState(1);
+
+  useEffect(() => {
+    // getBooks();
+  }, []);
+
+  const getBooks = async () => {
+    const url = `https://openlibrary.org/subjects/${subject}.json?limit=10?offset=${counter}`;
+    axios.get(url).then(res => console.log("respose", JSON.stringify(res))).catch(err => console.log("err", JSON.stringify(err.message)));
+  }
+
+  const data = [
+    {
+      name: "yos",
+      age: 21,
+      gender: "M",
+    },
+    {
+      name: "yos1",
+      age: 23,
+      gender: "M",
+    },
+    {
+      name: "yos2",
+      age: 12,
+      gender: "F",
+    },
+    {
+      name: "yos3",
+      age: 14,
+      gender: "M",
+    },
+  ];
   
-  return <div>{subject}</div>;
+  return (
+    <div>
+      {subject}
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Gender</th>
+        </tr>
+        {data.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.name}</td>
+              <td>{val.age}</td>
+              <td>{val.gender}</td>
+            </tr>
+          );
+        })}
+      </table>
+    </div>
+  );
 }
 
 export default Books;
